f71d07301546bcb5ac64272b3430e339adeba4af
Adithya Viswanathan <adithya.viswanathan@harness.io>
2023-11-17 16:28:00 +0530

    [feat]: [CDS-84609]: CRUD operations for webhooks at org and project level (#55494)
    * [feat]: [CDS-84603]: Added new mongo indexes for supporting webhooks at org and project
    
    * [feat]: [CDS-84603]: Updated the mongo index
    
    * [feat]: [CDS-84609]: Implementing CRUD webhooks operations for org and project
    
    * [feat]: [CDS-84609]: updated the get webhook call
    
    * [feat]: [CDS-84609]: updated the list webhook call
    
    * [feat]: [CDS-84609]: updated the list webhook call
    
    * [feat]: [CDS-84609]: updated the list webhook call
    
    * [feat]: [CDS-84609]: checkstyle fix
    
    * [feat]: [CDS-84609]: checkstyle fix
    
    * [feat]: [CDS-84609]: review comments incorporated
    
    * [feat]: [CDS-84609]: review comments incorporated
    
    * [feat]: [CDS-84609]: review comments incorporated
    
    (cherry picked from commit 6b34741ca1d05b0097985427a288b0e58121eebc)

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/gitxwebhooks/dtos/GitXEventsListRequestDTO.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/gitxwebhooks/dtos/GitXEventsListRequestDTO.java
@@ -12,6 +12,7 @@ import io.harness.annotations.dev.HarnessModuleComponent;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.ProductModule;
+import io.harness.beans.Scope;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -29,7 +30,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @OwnedBy(HarnessTeam.PIPELINE)
 public class GitXEventsListRequestDTO {
-  String accountIdentifier;
+  Scope scope;
   @Setter String webhookIdentifier;
   Long eventStartTime;
   Long eventEndTime;
