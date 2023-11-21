479ea78c01f54a50b5bdb1b8369e5c32beb29159
Anurag Madnawat <anurag.madnawat@harness.io>
2023-11-21 13:32:31 -0800

    Update ingesters.go

--- product/ci/addon/gojunit/ingesters.go
+++ product/ci/addon/gojunit/ingesters.go
@@ -1,6 +1,6 @@
 // Copyright Josh Komoroske. All rights reserved.
 // Use of this source code is governed by the MIT license,
-// a copy of which can be found in the LICENSE.txt file.
+// a copy of which can be found in the LICENSE.txt file. 
 
 package junit
 
