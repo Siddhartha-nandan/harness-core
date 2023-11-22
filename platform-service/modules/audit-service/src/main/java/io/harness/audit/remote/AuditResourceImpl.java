bf9103ba6364819860a2694add1e81b5a7127e56
Prateek Barapatre <Prateek.Barapatre@harness.io>
2023-11-23 00:58:54 +0530

    Revert "fix: [PL-42159]: add validation based on sort feild to prevent timeouts (#55147)"
    This reverts commit fe9985bdae4987d428b8cbf4f1ad0e2b107e1a66.

--- platform-service/modules/audit-service/src/main/java/io/harness/audit/remote/AuditResourceImpl.java
+++ platform-service/modules/audit-service/src/main/java/io/harness/audit/remote/AuditResourceImpl.java
@@ -22,7 +22,6 @@ import io.harness.audit.beans.ResourceScopeDTO;
 import io.harness.audit.entities.AuditEvent.AuditEventKeys;
 import io.harness.audit.mapper.AuditEventMapper;
 import io.harness.beans.SortOrder;
-import io.harness.exception.InvalidRequestException;
 import io.harness.ng.beans.PageRequest;
 import io.harness.ng.beans.PageResponse;
 import io.harness.ng.core.dto.ResponseDTO;
@@ -70,9 +69,6 @@ public class AuditResourceImpl implements AuditResource {
     if (isEmpty(pageRequest.getSortOrders())) {
       SortOrder order = SortOrder.Builder.aSortOrder().withField(AuditEventKeys.timestamp, DESC).build();
       pageRequest.setSortOrders(ImmutableList.of(order));
-    } else if (pageRequest.getSortOrders().size() > 1
-        || !AuditEventKeys.timestamp.equals(pageRequest.getSortOrders().get(0).getFieldName())) {
-      throw new InvalidRequestException("Sorting is supported only on timestamp.");
     }
     Page<AuditEventDTO> audits =
         auditService.list(accountIdentifier, pageRequest, auditFilterPropertiesDTO).map(AuditEventMapper::toDTO);
