85d3632e336351f03d60a93563c57318d0c703ba
tuodor.macari@harness.io <tudor.macari@harness.io>
2023-11-28 16:27:01 +0200

    feat: [CDS-84974]: formatting

--- 125-cd-nextgen/src/test/java/io/harness/cdng/metrics/NextGenManagerMetricsUtilsTest.java
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/metrics/NextGenManagerMetricsUtilsTest.java
@@ -1,10 +1,15 @@
 package io.harness.cdng.metrics;
 
+import static io.harness.rule.OwnerRule.TMACARI;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
 import io.harness.CategoryTest;
 import io.harness.category.element.UnitTests;
-
 import io.harness.metrics.service.api.MetricService;
 import io.harness.rule.Owner;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -13,26 +18,19 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import static io.harness.rule.OwnerRule.TMACARI;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-
 public class NextGenManagerMetricsUtilsTest extends CategoryTest {
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
+  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-    @Mock
-    private MetricService metricService;
+  @Mock private MetricService metricService;
 
-    @InjectMocks
-    NextGenManagerMetricsUtils nextGenManagerMetricsUtils;
+  @InjectMocks NextGenManagerMetricsUtils nextGenManagerMetricsUtils;
 
-    @Test
-    @Owner(developers = TMACARI)
-    @Category(UnitTests.class)
-    public void test() {
-        nextGenManagerMetricsUtils.publishArtifactCounterMetrics("accountId", "status");
+  @Test
+  @Owner(developers = TMACARI)
+  @Category(UnitTests.class)
+  public void test() {
+    nextGenManagerMetricsUtils.publishArtifactCounterMetrics("accountId", "status");
 
-        verify(metricService).incCounter(eq("artifacts_counter"));
-    }
+    verify(metricService).incCounter(eq("artifacts_counter"));
+  }
 }
