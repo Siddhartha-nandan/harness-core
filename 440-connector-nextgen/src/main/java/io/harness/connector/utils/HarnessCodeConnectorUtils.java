f3d29cf5a7c9f1897ef6c10933815cef3b257a46
Abhinav Singh <abhinav.singh@harness.io>
2023-12-06 23:26:11 +0530

    feat: [CODE-1151]: repo list changes for harness code

--- 440-connector-nextgen/src/main/java/io/harness/connector/utils/HarnessCodeConnectorUtils.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/utils/HarnessCodeConnectorUtils.java
@@ -7,10 +7,13 @@
 
 package io.harness.connector.utils;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 import io.harness.delegate.beans.connector.scm.GitConnectionType;
 import io.harness.delegate.beans.connector.scm.harness.HarnessApiAccessDTO;
 import io.harness.delegate.beans.connector.scm.harness.HarnessApiAccessType;
 import io.harness.delegate.beans.connector.scm.harness.HarnessConnectorDTO;
+import io.harness.delegate.beans.connector.scm.harness.HarnessConnectorDTO.HarnessConnectorDTOBuilder;
 import io.harness.delegate.beans.connector.scm.harness.HarnessJWTTokenSpecDTO;
 import io.harness.encryption.SecretRefData;
 import io.harness.git.GitClientHelper;
@@ -28,14 +31,20 @@ public class HarnessCodeConnectorUtils {
       String projectId, String serviceSecret, String harnessCodeApiBaseUrl) {
     SecretRefData token = SecretRefData.builder().decryptedValue(getToken(serviceSecret).toCharArray()).build();
     HarnessJWTTokenSpecDTO jwtTokenSpecDTO = HarnessJWTTokenSpecDTO.builder().tokenRef(token).build();
-    return HarnessConnectorDTO.builder()
-        .connectionType(GitConnectionType.REPO)
-        .apiAccess(HarnessApiAccessDTO.builder().spec(jwtTokenSpecDTO).type(HarnessApiAccessType.JWT_TOKEN).build())
-        .connectionType(GitConnectionType.REPO)
-        .executeOnDelegate(false)
-        .slug(GitClientHelper.convertToHarnessRepoName(accountId, orgId, projectId, repoName) + "/+")
-        .apiUrl(harnessCodeApiBaseUrl)
-        .build();
+    HarnessConnectorDTOBuilder harnessConnectorDTOBuilder =
+        HarnessConnectorDTO.builder()
+            .connectionType(GitConnectionType.REPO)
+            .apiAccess(HarnessApiAccessDTO.builder().spec(jwtTokenSpecDTO).type(HarnessApiAccessType.JWT_TOKEN).build())
+            .connectionType(GitConnectionType.REPO)
+            .executeOnDelegate(false)
+            .apiUrl(harnessCodeApiBaseUrl);
+    if (isEmpty(repoName)) {
+      harnessConnectorDTOBuilder.accountId(accountId).orgId(orgId).projectId(projectId);
+    } else {
+      harnessConnectorDTOBuilder.slug(
+          GitClientHelper.convertToHarnessRepoName(accountId, orgId, projectId, repoName) + "/+");
+    }
+    return harnessConnectorDTOBuilder.build();
   }
 
   private String getToken(String serviceSecret) {
