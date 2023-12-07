4578154232a749b6acf4e191e66b03667e37a5d0
Aashrit Garg <139942493+Aashrit-Harness@users.noreply.github.com>
2023-12-07 13:57:40 +0530

    feat: [IDP-1759]: Dynatrace Plugin HF (#56457)

--- idp-service/src/main/java/io/harness/idp/migration/IdpBGMigrationDetails.java
+++ idp-service/src/main/java/io/harness/idp/migration/IdpBGMigrationDetails.java
@@ -84,6 +84,7 @@ public class IdpBGMigrationDetails implements MigrationDetails {
         .add(Pair.of(50, PluginInfoMigration.class))
         .add(Pair.of(51, PluginsInfoCreatedByMigration.class))
         .add(Pair.of(52, PluginInfoMigration.class))
+        .add(Pair.of(53, PluginInfoMigration.class))
         .build();
   }
 }
