788a1c21334fa71e5654eae7ce59f0d71d26a604
Yuvraj Singhal <yuvraj.singhal@harness.io>
2023-12-13 02:00:23 +0530

    feat: [CDS-86777]: Add caching to avoid displaying active count for zero executions

--- pipeline-service/modules/orchestration/src/main/java/io/harness/engine/executions/plan/PlanExecutionMonitorServiceImpl.java
+++ pipeline-service/modules/orchestration/src/main/java/io/harness/engine/executions/plan/PlanExecutionMonitorServiceImpl.java
@@ -6,6 +6,7 @@
  */
 
 package io.harness.engine.executions.plan;
+
 import io.harness.annotations.dev.CodePulse;
 import io.harness.annotations.dev.HarnessModuleComponent;
 import io.harness.annotations.dev.HarnessTeam;
@@ -16,11 +17,16 @@ import io.harness.monitoring.ExecutionCountWithAccountResult;
 import io.harness.pms.events.PmsEventMonitoringConstants;
 import io.harness.pms.events.base.PmsMetricContextGuard;
 
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.cache.Cache;
 
 @CodePulse(module = ProductModule.CDS, unitCoverageRequired = true, components = {HarnessModuleComponent.CDS_PIPELINE})
@@ -28,16 +34,21 @@ import javax.cache.Cache;
 @Singleton
 public class PlanExecutionMonitorServiceImpl implements PlanExecutionMonitorService {
   private static final String PIPELINE_EXECUTION_ACTIVE_COUNT = "pipeline_execution_active_count";
+  public static final String PLAN_EXECUTION = "_plan_execution";
   private final PlanExecutionService planExecutionService;
   private final MetricService metricService;
   private final Cache<String, Integer> metricsCache;
+  private final LoadingCache<String, Set<String>>
+      metricsLoadingCache; // Cache to avoid retaining count in prometheus for a key in case count is 0 now
 
   @Inject
   public PlanExecutionMonitorServiceImpl(PlanExecutionService planExecutionService, MetricService metricService,
-      @Named("pmsMetricsCache") Cache<String, Integer> metricsCache) {
+      @Named("pmsMetricsCache") Cache<String, Integer> metricsCache,
+      @Named("pmsMetricsLoadingCache") LoadingCache<String, Set<String>> metricsLoadingCache) {
     this.planExecutionService = planExecutionService;
     this.metricService = metricService;
     this.metricsCache = metricsCache;
+    this.metricsLoadingCache = metricsLoadingCache;
   }
 
   @Override
@@ -47,16 +58,37 @@ public class PlanExecutionMonitorServiceImpl implements PlanExecutionMonitorServ
       return;
     }
 
-    for (ExecutionCountWithAccountResult accountResult :
-        planExecutionService.aggregateRunningExecutionCountPerAccount()) {
-      Map<String, String> metricContextMap =
-          ImmutableMap.<String, String>builder()
-              .put(PmsEventMonitoringConstants.ACCOUNT_ID, accountResult.getAccountId())
-              .build();
+    List<ExecutionCountWithAccountResult> accountResultList =
+        planExecutionService.aggregateRunningExecutionCountPerAccount();
+    for (ExecutionCountWithAccountResult accountResult : accountResultList) {
+      populateMetric(PmsEventMonitoringConstants.ACCOUNT_ID, accountResult.getAccountId(), accountResult.getCount());
+    }
+    populateCountForMissingKeysInCurrentExecutionStats(accountResultList);
+  }
 
-      try (PmsMetricContextGuard pmsMetricContextGuard = new PmsMetricContextGuard(metricContextMap)) {
-        metricService.recordMetric(PIPELINE_EXECUTION_ACTIVE_COUNT, accountResult.getCount());
-      }
+  private void populateCountForMissingKeysInCurrentExecutionStats(
+      List<ExecutionCountWithAccountResult> accountResultList) {
+    Set<String> currentAccountIds = new HashSet<>();
+    for (ExecutionCountWithAccountResult accountResult : accountResultList) {
+      currentAccountIds.add(accountResult.getAccountId());
+    }
+    populateZeroCount(currentAccountIds, PmsEventMonitoringConstants.ACCOUNT_ID);
+  }
+
+  private void populateZeroCount(Set<String> currentKeys, String metricKey) {
+    Set<String> cachedKeys = metricsLoadingCache.get(metricKey + PLAN_EXECUTION);
+    Set<String> zeroCountKeys = Sets.difference(cachedKeys, currentKeys);
+    for (String key : zeroCountKeys) {
+      populateMetric(metricKey, key, 0);
+    }
+    cachedKeys.addAll(currentKeys);
+  }
+
+  private void populateMetric(String key, String keyValue, Integer metricValue) {
+    Map<String, String> metricContextMap = ImmutableMap.<String, String>builder().put(key, keyValue).build();
+
+    try (PmsMetricContextGuard pmsMetricContextGuard = new PmsMetricContextGuard(metricContextMap)) {
+      metricService.recordMetric(PIPELINE_EXECUTION_ACTIVE_COUNT, metricValue);
     }
   }
 }
