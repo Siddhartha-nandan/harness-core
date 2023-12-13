6b8bfe401a142d0bb9ad8dd4b7ca1c93ee73f5a4
Dev Mittal <devki.mittal@harness.io>
2023-12-13 20:25:53 +0530

    fix: [CI-8872]: upgrade scm version (#56721)
    * fix: [CI-8872]: upgrade scm version
    
    * bump delegate version

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-  DEFAULT("9f1eaa9f");
+  DEFAULT("80f7e024");
 
   private final String version;
 
