fde254a268fc38b8cceaf48c4aa159a2dafdd8c5
Sarthak Kasat <sarthak.kasat@harness.io>
2023-12-13 20:20:52 +0530

    feat: [CDS-86691]: FilterCreator for Approval Stage V1

---
+++ pipeline-service/modules/orchestration-steps/contracts/src/main/java/io/harness/steps/approval/stage/v1/ApprovalStageNodeV1.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.steps.approval.stage.v1;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.plancreator.stages.stage.v1.AbstractStageNodeV1;
+import io.harness.pms.yaml.ParameterField;
+import io.harness.pms.yaml.YAMLFieldNameConstants;
+import io.harness.yaml.core.failurestrategy.v1.FailureConfigV1;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.List;
+import lombok.Value;
+
+@Value
+@JsonTypeName(YAMLFieldNameConstants.APPROVAL_V1)
+@OwnedBy(PIPELINE)
+public class ApprovalStageNodeV1 extends AbstractStageNodeV1 {
+  String type = YAMLFieldNameConstants.APPROVAL_V1;
+  ApprovalStageConfigV1 spec;
+  @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY) ParameterField<List<FailureConfigV1>> failure;
+
+  @Override
+  public String getType() {
+    return YAMLFieldNameConstants.APPROVAL_V1;
+  }
+}
--- pipeline-service/service/src/test/java/io/harness/pms/plan/creation/PipelineServiceInternalInfoProviderTest.java
+++ pipeline-service/service/src/test/java/io/harness/pms/plan/creation/PipelineServiceInternalInfoProviderTest.java
@@ -26,6 +26,7 @@ import io.harness.filters.ParallelFilterJsonCreator;
 import io.harness.filters.PipelineFilterJsonCreator;
 import io.harness.plancreator.approval.ApprovalStageFilterJsonCreator;
 import io.harness.plancreator.approval.ApprovalStagePlanCreatorV2;
+import io.harness.plancreator.approval.v1.ApprovalStageFilterCreator;
 import io.harness.plancreator.group.GroupPlanCreatorV1;
 import io.harness.plancreator.pipeline.NGPipelinePlanCreator;
 import io.harness.plancreator.stages.StagesPlanCreator;
@@ -103,7 +104,7 @@ import org.mockito.MockitoAnnotations;
 @OwnedBy(PIPELINE)
 public class PipelineServiceInternalInfoProviderTest extends CategoryTest {
   public static final int PLAN_CREATOR_NUMBER = 37;
-  public static final int FILTER_JSON_CREATOR_NUMBER = 14;
+  public static final int FILTER_JSON_CREATOR_NUMBER = 15;
   public static final int VARIABLE_CREATOR_NUMBER = 27;
 
   @InjectMocks PipelineServiceInternalInfoProvider pipelineServiceInternalInfoProvider;
@@ -187,6 +188,7 @@ public class PipelineServiceInternalInfoProviderTest extends CategoryTest {
     assertThat(filterCreatorClasses.contains(GroupFilterJsonCreator.class)).isTrue();
     assertThat(filterCreatorClasses.contains(HarnessApprovalStepFilterJsonCreatorV2.class)).isTrue();
     assertThat(filterCreatorClasses.contains(CdSscaStepFilterJsonCreator.class)).isTrue();
+    assertThat(filterCreatorClasses.contains(ApprovalStageFilterCreator.class)).isTrue();
   }
 
   @Test
