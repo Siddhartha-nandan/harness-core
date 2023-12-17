e0e13d1fa643dba632f516d5336e599c14e99f1c
Sarthak Kasat <sarthak.kasat@harness.io>
2023-12-17 17:39:28 +0530

    feat: [CDS-86933]: Harness Approval Step POJO

---
+++ pipeline-service/modules/orchestration-steps/src/main/java/io/harness/steps/approval/step/harness/v1/HarnessApprovalStepNodeV1.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.steps.approval.step.harness.v1;
+
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+import io.harness.annotations.dev.CodePulse;
+import io.harness.annotations.dev.HarnessModuleComponent;
+import io.harness.annotations.dev.ProductModule;
+import io.harness.plancreator.steps.common.SpecParameters;
+import io.harness.plancreator.steps.internal.v1.PmsAbstractStepNodeV1;
+import io.harness.steps.StepSpecTypeConstantsV1;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Builder;
+import lombok.Getter;
+
+@CodePulse(
+    module = ProductModule.CDS, unitCoverageRequired = false, components = {HarnessModuleComponent.CDS_COMMON_STEPS})
+@Builder
+@JsonTypeName(StepSpecTypeConstantsV1.HARNESS_APPROVAL)
+public class HarnessApprovalStepNodeV1 extends PmsAbstractStepNodeV1 {
+  @Getter String type = StepSpecTypeConstantsV1.HARNESS_APPROVAL;
+
+  @Getter
+  @JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
+  HarnessApprovalStepInfoV1 spec;
+
+  @Override
+  public SpecParameters getSpecParameters() {
+    return HarnessApprovalStepParameters.infoBuilder()
+        .approvers(spec.getApprovers())
+        .message(spec.getMessage())
+        .auto_approval(spec.getAuto_approval())
+        .auto_reject(spec.getAuto_reject())
+        .callbackId(spec.getCallbackId())
+        .include_execution_history(spec.getInclude_execution_history())
+        .inputs(spec.getInputs())
+        .build();
+  }
+}
---
+++ pipeline-service/modules/orchestration-steps/src/main/java/io/harness/steps/approval/step/harness/v1/HarnessApprovalStepParameters.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.steps.approval.step.harness.v1;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.plancreator.steps.common.SpecParameters;
+import io.harness.pms.yaml.HarnessYamlVersion;
+import io.harness.pms.yaml.ParameterField;
+import io.harness.steps.approval.step.harness.beans.ApproverInputInfo;
+import io.harness.steps.approval.step.harness.beans.Approvers;
+import io.harness.steps.approval.step.harness.beans.AutoApprovalParams;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+
+@OwnedBy(PIPELINE)
+@Data
+@Builder(builderMethodName = "infoBuilder")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class HarnessApprovalStepParameters implements SpecParameters {
+  @NotNull ParameterField<String> message;
+  ParameterField<String> callbackId;
+  @NotNull ParameterField<Boolean> include_execution_history;
+  AutoApprovalParams auto_approval;
+  @NotNull Approvers approvers;
+  List<ApproverInputInfo> inputs;
+  ParameterField<Boolean> auto_reject;
+
+  @Override
+  public String getVersion() {
+    return HarnessYamlVersion.V1;
+  }
+}
