86787141825d9f856ba6d9a6b31861992c2bee50
Abraham Gutierrez <abraham.gutierrez@harness.io>
2023-12-21 15:06:18 -0600

    Reverted unexpected changes toBambooBuildSpecParameters.java

--- 125-cd-nextgen/src/main/java/io/harness/cdng/bamboo/BambooBuildSpecParameters.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/bamboo/BambooBuildSpecParameters.java
@@ -28,8 +28,13 @@ import org.springframework.data.annotation.TypeAlias;
 @Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@TypeAlias("cetQualityGateStepParameters")
-@RecasterAlias("io.harness.cdng.cetqualitygatestep.CETQualityGateStepParameters")
+@TypeAlias("bambooBuildSpecParameters")
+@RecasterAlias("io.harness.cdng.bamboo.BambooBuildSpecParameters")
 public class BambooBuildSpecParameters implements SpecParameters {
+  @NotEmpty ParameterField<String> connectorRef;
+  @NotEmpty ParameterField<String> planName;
   Map<String, ParameterField<String>> fields;
+  private Map<String, String> filePathsForAssertion;
+  private String queuedBuildUrl;
+  ParameterField<List<TaskSelectorYaml>> delegateSelectors;
 }
