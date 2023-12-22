a083bcfd5cf12bf3a81f0a7ce6e63206dc71fbbf
Yogesh Chauhan <yogesh.chauhan@harness.io>
2023-12-22 15:30:02 +0530

    techdebt: [CDS-83769]: Revert changes in InstrumentationHelper bcoz our checkstyle is outdated

--- 970-telemetry-beans/src/main/java/io/harness/telemetry/helpers/InstrumentationHelper.java
+++ 970-telemetry-beans/src/main/java/io/harness/telemetry/helpers/InstrumentationHelper.java
@@ -37,16 +37,20 @@ public class InstrumentationHelper {
     String identity = null;
     if (principal != null) {
       switch (principal.getType()) {
-        case USER -> {
+        case USER:
           UserPrincipal userPrincipal = (UserPrincipal) principal;
           identity = userPrincipal.getEmail();
-        }
-        case SERVICE_ACCOUNT -> {
+          break;
+        case SERVICE_ACCOUNT:
           ServiceAccountPrincipal serviceAccountPrincipal = (ServiceAccountPrincipal) principal;
           identity = serviceAccountPrincipal.getEmail();
-        }
-        case API_KEY, SERVICE -> identity = principal.getName();
-        default -> log.warn("Unknown principal type from SecurityContextBuilder when reading identity");
+          break;
+        case API_KEY:
+        case SERVICE:
+          identity = principal.getName();
+          break;
+        default:
+          log.warn("Unknown principal type from SecurityContextBuilder when reading identity");
       }
     }
     if (isEmpty(identity)) {
