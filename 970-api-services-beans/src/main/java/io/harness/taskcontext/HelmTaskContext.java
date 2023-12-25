67622efd027c7a43b36e96cc769ff91124ab6041
Marko Lukic <marko.lukic@harness.io>
2023-12-25 13:01:02 +0100

    [CDS-85972]: introducing abstract task context

--- 970-api-services-beans/src/main/java/io/harness/taskcontext/HelmTaskContext.java
+++ 970-api-services-beans/src/main/java/io/harness/taskcontext/HelmTaskContext.java
@@ -10,49 +10,9 @@ package io.harness.taskcontext;
 import io.harness.annotations.dev.CodePulse;
 import io.harness.annotations.dev.HarnessModuleComponent;
 import io.harness.annotations.dev.ProductModule;
-import io.harness.taskcontext.infra.InfraContext;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import lombok.Builder;
+import lombok.experimental.SuperBuilder;
 
-@Builder
+@SuperBuilder
 @CodePulse(module = ProductModule.CDS, unitCoverageRequired = true, components = {HarnessModuleComponent.CDS_K8S})
-public class HelmTaskContext implements TaskContext {
-  Set<String> hints;
-  InfraContext infraContext;
-
-  @Override
-  public Optional<String> getConnectorInfo() {
-    if (infraContext != null) {
-      return infraContext.getConnectorInfo();
-    }
-    return Optional.empty();
-  }
-
-  @Override
-  public void addHint(String hint) {
-    if (hints == null) {
-      hints = new HashSet<>();
-    }
-    hints.add(hint);
-  }
-
-  @Override
-  public void addHints(List<String> hints) {
-    if (this.hints == null) {
-      this.hints = new HashSet<>();
-    }
-    this.hints.addAll(hints);
-  }
-
-  @Override
-  public Set<String> getHints() {
-    if (hints == null) {
-      hints = new HashSet<>();
-    }
-    return hints;
-  }
-}
+public class HelmTaskContext extends AbstractTaskContext {}
