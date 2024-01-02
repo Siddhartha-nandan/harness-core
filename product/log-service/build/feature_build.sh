5cf881340f1b4bf54acb65b186e50a80896cfffa
abhay084 <abhay.ganvir@harness.io>
2024-01-02 11:40:43 +0530

    fix: [PL-43592]: log service test

--- product/log-service/build/feature_build.sh
+++ product/log-service/build/feature_build.sh
@@ -14,8 +14,8 @@ echo "OPTIMIZED_PACKAGE_TESTS=0" >> test-util.bzl
 
 apt-get update -y
 apt install curl -y
-curl -O https://dl.google.com/go/go1.19.8.linux-amd64.tar.gz
-tar -xvf go1.19.8.linux-amd64.tar.gz
+curl -O https://dl.google.com/go/go1.21.5.linux-amd64.tar.gz
+tar -xvf go1.21.5.linux-amd64.tar.gz
 mv go/ /usr/local/
 export GOROOT=/usr/local/go
 export GOPATH=/usr/local
