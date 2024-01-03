638f65dd44c2b34fbf182cd8ee2de7f17b8bbf98
Lucas Sales <lucas.sales@harness.io>
2024-01-02 21:53:35 -0300

    feat: [CDS-87602]: migrator infra scoped services option

--- 100-migrator/src/main/java/io/harness/ngmigration/service/entity/InfraMigrationService.java
+++ 100-migrator/src/main/java/io/harness/ngmigration/service/entity/InfraMigrationService.java
@@ -85,7 +85,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
